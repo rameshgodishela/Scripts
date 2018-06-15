816c7cda982286b1803f1303e49805d37d84957f
Ramesh Godishela <iamrameshjonathan@gmail.com>
2018-06-15 11:40:36 +0530

    updatefiles

--- powershell/Do-Stuff.ps1
+++
@@ -1,9 +0,0 @@
-Function Do-Stuff{
-[CmdletBinding(SupportsShouldProcess=$true)]
-    param([string[]]$Objects)
-ForEach($item in $Objects){
-        if ($pscmdlet.ShouldProcess("$item", "DoStuff")){
-            write-output "Actually performing `$Action on $item"
-            }
- }
- }
